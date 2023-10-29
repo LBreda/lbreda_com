7a7cd2afbb4a3b161eaa18edfd3675d88d18e445
Lorenzo Breda <lorenzo@lbreda.com>
2023-10-29 21:34:11 +0100

    :sparkles: Footer behavior

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -19,71 +19,75 @@
     <meta property="og:title" content="Lorenzo Breda - Home">
     <meta property="og:image" content="/imgs/banner.jpg">
     <meta property="og:description" content="Lorenzo Breda - Freelance Web Developer">
-    
+
     <!-- Mastodon -->
     <link rel="me" href="https://livellosegreto.it/@lbreda">
 </head>
 <body>
-<nav class="sticky top-0 x-50 bg-primary text-white" x-data="{ isMobileMenuShowing: false }">
-    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div class="relative flex items-center justify-between h-16">
-            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <!-- Mobile menu button-->
-                <button x-on:click="isMobileMenuShowing = !isMobileMenuShowing" type="button"
-                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                        aria-controls="mobile-menu" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M4 6h16M4 12h16M4 18h16"/>
-                    </svg>
-                    <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
-                    </svg>
-                </button>
-            </div>
+<div id="page-container">
+    <nav class="sticky top-0 x-50 bg-primary text-white" x-data="{ isMobileMenuShowing: false }">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="relative flex items-center justify-between h-16">
+                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    <!-- Mobile menu button-->
+                    <button x-on:click="isMobileMenuShowing = !isMobileMenuShowing" type="button"
+                            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                            aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M4 6h16M4 12h16M4 18h16"/>
+                        </svg>
+                        <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                             stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M6 18L18 6M6 6l12 12"/>
+                        </svg>
+                    </button>
+                </div>
 
-            <div class="flex items-center justify-end content-endm min-w-full sm:justify-start sm:content-start sm:min-w-min">
-                <h1 class="text-3xl">Lorenzo Breda</h1>
-            </div>
+                <div class="flex items-center justify-end content-endm min-w-full sm:justify-start sm:content-start sm:min-w-min">
+                    <h1 class="text-3xl">Lorenzo Breda</h1>
+                </div>
 
-            <div class="flex-1 flex items-center justify-end sm:items-stretch">
-                <div class="hidden sm:block sm:ml-6">
-                    <div class="flex space-x-4">
-                        @include("_partials.{$page->language}.menu")
+                <div class="flex-1 flex items-center justify-end sm:items-stretch">
+                    <div class="hidden sm:block sm:ml-6">
+                        <div class="flex space-x-4">
+                            @include("_partials.{$page->language}.menu")
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <!-- Mobile menu, show/hide based on menu state. -->
-    <div class="sm:hidden" id="mobile-menu" x-show="isMobileMenuShowing" x-transition>
-        <div class="px-2 pt-2 pb-3 space-y-1">
-            @include("_partials.{$page->language}.menu")
-        </div>
-    </div>
-</nav>
-
-@if($page->show_title ?? true)
-    <div class="bg-secondary">
-        <div class="container mx-auto h-32 flex flex-wrap content-evenly items-center px-2">
-            <div class="flex content-end">
-                <h2 class="sm:text-7xl text-2xl">
-                    {{ $page->title }}
-                </h2>
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div class="sm:hidden" id="mobile-menu" x-show="isMobileMenuShowing" x-transition>
+            <div class="px-2 pt-2 pb-3 space-y-1">
+                @include("_partials.{$page->language}.menu")
             </div>
         </div>
+    </nav>
+
+    @if($page->show_title ?? true)
+        <div class="bg-secondary">
+            <div class="container mx-auto h-32 flex flex-wrap content-evenly items-center px-2">
+                <div class="flex content-end">
+                    <h2 class="sm:text-7xl text-2xl">
+                        {{ $page->title }}
+                    </h2>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    <div id="page-body">
+        @yield('body')
     </div>
-@endif
 
-@yield('body')
-
-@include("_partials.{$page->language}.footer")
-@include("_partials.{$page->language}.cookie_banner")
-
+    @include("_partials.{$page->language}.footer")
+    @include("_partials.{$page->language}.cookie_banner")
+</div>
 <script type="application/ld+json">
         {
           "@context": "https://schema.org/",
@@ -146,6 +150,7 @@
             "url": https://www.twobeesolution.com/",
           }
         }
+
 </script>
 </body>
 </html>
--- source/assets/build/js/main.js
+++ source/assets/build/js/main.js
@@ -7429,21 +7429,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
-alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start(); // Google Analytics
-
-(function (i, s, o, g, r, a, m) {
-  i['GoogleAnalyticsObject'] = r;
-  i[r] = i[r] || function () {
-    (i[r].q = i[r].q || []).push(arguments);
-  }, i[r].l = 1 * new Date();
-  a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-ga('create', 'UA-137971-1', 'auto');
-ga('send', 'pageview'); // Matomo
+alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start(); // Matomo
 
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
