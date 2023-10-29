283200c806d63b843265be393af07c0eb8346970
Lorenzo Breda <lorenzo@lbreda.com>
2023-10-29 21:21:48 +0100

    :sparkles: Site description

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ return [
     'production' => true,
     'baseUrl' => 'https://www.lbreda.com',
     'title' => 'Lorenzo Breda',
-    'description' => 'Website description.',
+    'description' => "Lorenzo Breda's Personal Home Page",
     'collections' => [],'selected' => function ($page, $section) {
         return Str::contains($page->getPath(), $section) ? 'selected' : '';
     },
